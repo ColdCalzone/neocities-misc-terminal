745369e8572d2d9e2aaa39728510a19f95e0939d
ColdCalzone <coldhotpocket1@gmail.com>
2024-08-12 22:21:11 -0400

    Simplify Path somewhat

--- src/utils/path.rs
+++ src/utils/path.rs
@@ -1,28 +1,40 @@
-use std::fmt;
+use std::{
+    fmt,
+    ops::{Deref, DerefMut},
+};
 
-pub struct Path {
-    path: Vec<String>,
-}
+pub struct Path(Vec<String>);
 
 impl Path {
-    fn new(path: &str) -> Self {
-        Path {
-            path: path
-                .split('/')
+    pub fn new(path: &str) -> Self {
+        Path(
+            path.split('/')
                 .map(|x| x.to_owned())
                 .collect::<Vec<String>>(),
-        }
+        )
     }
 }
 
 impl fmt::Display for Path {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "/{}", self.path.join("/"))
+        write!(f, "/{}", self.join("/"))
     }
 }
 
 impl From<&str> for Path {
     fn from(value: &str) -> Self {
         Path::new(value)
+    }
+}
+
+impl Deref for Path {
+    type Target = Vec<String>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl DerefMut for Path {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
